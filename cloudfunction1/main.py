b5bfca7f8a814991858eec7f30f6792ad5478769
Babita kumari <babita.kumari@atgeirsolutions.com>
2024-10-20 12:31:29 +0530

    new

--- cloudfunction1/main.py
+++ cloudfunction1/main.py
@@ -19,5 +19,5 @@ def hello_http(request):
     elif request_args and 'name' in request_args:
         name = request_args['name']
     else:
-        name = 'ndkjqsaoaWorld!!!!!'
+        name = 'sahdasjhgdjhagdsfndkjqsaoaWorld!!!!!'
     return 'Hello {}!'.format(name)
